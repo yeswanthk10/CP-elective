fa2be4568de7c49f52a98b18956036bea2c6a4b4
yeswanthk10 <71495494+yeswanthk10@users.noreply.github.com>
2021-08-10 21:30:54 +0530

    commit

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -6,4 +6,10 @@
 
 
 def fun_isfactor(f, n):
-	return False # replace with your solution
+	if(f==0 and n==0):
+		return True # replace with your solution
+	elif(f==0):
+		return False
+	elif(n%f==0):
+		return True
+	return False
