b889e11dacd9ea9fc0d70be908e25de8097d9c58
yeswanthk10 <71495494+yeswanthk10@users.noreply.github.com>
2021-08-12 09:07:38 +0530

    commit

--- 04-pythondictionaries-Python/pythondictionaries.py
+++ 04-pythondictionaries-Python/pythondictionaries.py
@@ -33,11 +33,21 @@ Asian City - Country"""
 
 def sortUSA():
     '''Return all the cities in the USA in alphabetical order'''
-    pass
+    #pass
+    return sorted(locations['North America']['USA'])
 
 def alphaAsia():
     '''Return all the cities in Asia continent in alphabetical order'''
-    pass
+    #pass
+    cities = list()
+    for i in locations['Asia']:
+        for j in locations['Asia'][i]:
+            cities.extend([ j+' - '+i])
+    return sorted(cities)
 
 # Note: Check for test cases to understand the output format.
 locations = {'North America': {'USA': ['Mountain View']}}
+locations['Asia'] = {'India' : ['Bangalore']}
+locations['North America']['USA'].append('Atlanta')
+locations['Africa'] = {'Egypt' : ['Cairo']}
+locations['Asia']['China'] = ['Shanghai']
