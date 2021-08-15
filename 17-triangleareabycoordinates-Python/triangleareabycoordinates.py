d1507cfd5583935a3c77f2258771c42a98caef33
yeswanthk10 <71495494+yeswanthk10@users.noreply.github.com>
2021-08-15 22:32:08 +0530

    commit

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -4,6 +4,7 @@
 # area of the triangle formed by those three points. Hint: you should make constructive use of
 # the triangleArea function you just wrote above.
 
+import math
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
-	# your code goes here
-	pass
+	formula = (0.5)*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))
+	return formula
