be5bc961604908ef6fe47698a891c83e100e7997
yeswanthk10 <71495494+yeswanthk10@users.noreply.github.com>
2021-08-15 22:40:05 +0530

    commit

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -3,5 +3,17 @@
 # that occurs most frequently in it, with ties going to the smaller digit.
 
 def mostfrequentdigit(n):
-	# your code goes here
-	pass
+    a = abs(n)
+    if a == 0:
+        return 0
+    else:
+        while(a>0):
+            num=a
+            res1 = num%10
+            num = num//10
+            res2= num%10
+            if(res1 != res2 and a>9):
+                a=a//10
+            else:
+                return res1
+        return min(res1,res2)
