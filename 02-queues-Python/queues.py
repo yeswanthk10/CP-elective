d78d7608ac278e1a157ef6173840fd9f03dc515d
yeswanthk10 <71495494+yeswanthk10@users.noreply.github.com>
2021-08-15 22:16:13 +0530

    commit

--- 02-queues-Python/queues.py
+++ 02-queues-Python/queues.py
@@ -9,10 +9,10 @@ class Queue:
         self.storage = [head]
 
     def enqueue(self, new_element):
-        pass
+        self.storage.append(new_element)
 
     def peek(self):
-        pass 
+        return self.storage[0]
 
     def dequeue(self):
-        pass
+        return self.storage.pop(0)
