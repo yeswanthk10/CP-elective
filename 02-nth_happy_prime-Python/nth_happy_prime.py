6c7b0dafd9fec928709b1b243cfbc026e15b0321
yeswanthk10 <71495494+yeswanthk10@users.noreply.github.com>
2021-08-19 21:30:57 +0530

    commit

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -4,5 +4,39 @@
 
 
 
+def isprime(n):
+    if (n < 2):
+        return False
+    if (n==2):
+        return True
+    else:
+        for i in range(2,int(n)):
+            if(n%i==0):
+                return False
+    return True
+
+def ishappynumber(n):
+    
+    if n<1:
+        return False
+    sum = 0
+    while(n>0):
+        sum+= (n % 10) * (n % 10)
+        n = n//10
+    if sum == 1:
+        return True 
+    elif sum == 4:
+        return False
+    else:
+        return ishappynumber(sum)
+
 def fun_nth_happy_prime(n):
-	return 0
+    found = 0
+    guess = 0
+    while (found <= n):
+        guess += 1
+        
+        if ((ishappynumber(guess)) and (isprime(guess))):
+            found += 1
+            
+    return guess
