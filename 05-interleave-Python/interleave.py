ef7b0b3e05c615e3e9dff359b5aed8659ec25eca
yeswanthk10 <71495494+yeswanthk10@users.noreply.github.com>
2021-08-19 22:25:14 +0530

    commit

--- 05-interleave-Python/interleave.py
+++ 05-interleave-Python/interleave.py
@@ -8,5 +8,14 @@
 
 
 def fun_interleave(s1,s2):
-	return ""
+    s=""
+    if(len(s1)>len(s2)):
+        for i in range(len(s2)):
+            s+=s1[i]+s2[i]
+        s+=s1[len(s2):]
+    else:
+        for i in range(len(s1)):
+            s+=s1[i]+s2[i]
+        s+=s2[len(s1):]
+    return s
 	
