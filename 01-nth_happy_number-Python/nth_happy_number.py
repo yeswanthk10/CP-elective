5214018496a362a66570110f845447a55227d447
yeswanthk10 <71495494+yeswanthk10@users.noreply.github.com>
2021-08-19 20:40:44 +0530

    commit

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -15,5 +15,22 @@
 # assert(nth_happy_number(8) == 31)
 
 
+def Happynum(n):
+	sum = 0
+	while(n!=0):
+		sum += (n%10)**2
+		n//=10
+	if sum == 1:
+		return True
+	elif sum<10:
+		return False
+	else:
+		return Happynum(sum)
 def nth_happy_number(n):
-	return 0
+	f = 1
+	g = 0
+	while(f<=abs(n)):
+		g+=1
+		if(Happynum(g)):
+			f+=1
+	return g
