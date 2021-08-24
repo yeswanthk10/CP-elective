5d71e2100aed293651c19a6db8b296f79bae13ad
yeswanthk10 <71495494+yeswanthk10@users.noreply.github.com>
2021-08-24 17:07:08 +0530

    commit

--- 07-isfactorish-Python/isfactorish.py
+++ 07-isfactorish-Python/isfactorish.py
@@ -11,6 +11,36 @@
 #  assert(fun_isfactorish(42) == False) # 42 has a leading 0 (only 2 unique digits)
 
 
+def noofDigits(n):
+    c = 0
+    while(n > 0):
+        n = n//10
+        c += 1
+    return c
+
+
 def fun_isfactorish(n):
-	return False
+    t = n
+    if (n < 0):
+        n = -n
+
+    if(noofDigits(n) != 3 ):
+        return False
+
+    d1 = n % 10
+    n = n // 10
+    d2 = n % 10
+    n = n // 10
+    d3 = n % 10
+
+    if(d1 == 0 or d2 == 0 and d3 == 0):
+        return (False)
+
+    if(d1 == d2 or d2 == d3 or d3 == d1):
+        return (False)
+
+    if (t%d1 == 0 and t%d2 == 0 and t%d3 == 0):
+        return True
+
+    return (False) 
 
