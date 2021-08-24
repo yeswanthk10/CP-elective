5da3c87c3535eb5435a742481e73ec9a8efa99d8
yeswanthk10 <71495494+yeswanthk10@users.noreply.github.com>
2021-08-24 21:14:13 +0530

    commit

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -6,5 +6,22 @@
 # is a rotation of itself.
 
 def isrotation(x, y):
-	# Your code goes here
-	pass
+    # Your code goes here
+    s=str(x)
+    r=str(y)
+    rev=r[::-1]
+    if s==r:
+        return True
+    else:
+        
+        if len(s)==len(r):
+            res=s[len(s)-2:]+s[0:len(s)-2]
+            
+            if res==r:
+                return True
+            elif s==rev:
+                return True
+            else:
+                return False      
+        else:
+            return False
