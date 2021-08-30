d8a8339268153ee077d53216bcc4d0373678c604
yeswanthk10 <71495494+yeswanthk10@users.noreply.github.com>
2021-08-30 21:09:01 +0530

    Update non_destructive_remove_repeats.py

--- non_destructive_remove_repeats.py
+++ non_destructive_remove_repeats.py
@@ -10,7 +10,13 @@
 
 def nondestructiveRemoveRepeats(L):
     # Your code goes here...
-    pass
+    res=[]
+    for i in range(len(L)):
+        if L[i] not in res:
+            res.append(L[i])
+    return res
+
+
 
 assert(nondestructiveRemoveRepeats([1, 3, 5, 3, 3, 2, 1, 7, 5]) == [1, 3, 5, 2, 7])
 assert(nondestructiveRemoveRepeats([1, 5, 3, 3, 2, 1, 7, 5]) == [1, 5, 3, 2, 7])
