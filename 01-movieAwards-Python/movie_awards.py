5b6f8e600d7853db404e0ff6522aa1f68ca8e664
yeswanthk10 <71495494+yeswanthk10@users.noreply.github.com>
2021-09-04 16:24:17 +0530

    Update movie_awards.py

--- 01-movieAwards-Python/movie_awards.py
+++ 01-movieAwards-Python/movie_awards.py
@@ -21,4 +21,10 @@ the function should return as follows
 
 def movie_awards(oscarResults):
     # Your code goes here...
-    pass
+    d={}
+    for i in oscarResults:
+        if i[1] not in d:
+            d[i[1]]=1
+        else:
+            d[i[1]]+=1
+    return d
